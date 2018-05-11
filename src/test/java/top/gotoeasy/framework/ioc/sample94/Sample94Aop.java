022b8e707424f27d7ec902282acccf0081b40fee
zhangming <sunmoon2222@163.com>
2018-05-11 09:02:58 +0800

    log

--- src/test/java/top/gotoeasy/framework/ioc/sample94/Sample94Aop.java
+++ src/test/java/top/gotoeasy/framework/ioc/sample94/Sample94Aop.java
@@ -3,15 +3,17 @@ package top.gotoeasy.framework.ioc.sample94;
 import top.gotoeasy.framework.aop.annotation.Aop;
 import top.gotoeasy.framework.core.log.Log;
 import top.gotoeasy.framework.core.log.LoggerFactory;
-import top.gotoeasy.framework.ioc.sample93.Sample93Bean1;
 
 @Aop
 public class Sample94Aop {
 
-    private static final Log log = LoggerFactory.getLogger(Sample93Bean1.class);
+    private static final Log log = LoggerFactory.getLogger(Sample94Aop.class);
 
     private Sample94Aop() {
         log.debug("Sample94Aop");
     }
 
+    public String hello(String name) {
+        return "Hello " + name;
+    }
 }
